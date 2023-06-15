448966e413d0ef9b78190d337f4d846e52fe2981
Ahmad Alhussin <ahmad.alhusein2@gmail.com>
2023-06-15 18:23:58 +0300

    enhanced main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import MainView from './components/MainView';
 import NavBar from './components/NavBar';
 import Options from './components/Options';
+import Recents from './components/Recents';
+import Footer from './components/Footer';
+import WhyUs from './components/WhyUs';
 
 function App() {
+  const RECENT_POSTS_URL = "http://localhost:3030/recent-posts";
+  const DERLIVERED_POSTS_URL = "http://localhost:3030/delivered-posts";
+
   return (
     <div className="App">
       <NavBar />
       <MainView />
       <Options />
+      <WhyUs/>
+      {/* <div className="seperate"></div> */}
+      <Recents URL={RECENT_POSTS_URL} done={false} />
+      <div className="seperate"></div>
+      <Recents URL={DERLIVERED_POSTS_URL} done={true} />
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/Recents.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import HorizontalBlogs from './HorizontalBlogs';
+
+const Recents = ({ URL, done }) => {
+
+    const [posts, setPosts] = useState([]);
+    const [postIdx, setPostIdx] = useState(0);
+
+    useEffect(() => {
+        fetch(URL)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setPosts(data)
+            })
+            .catch((error) => {
+                for (let i = 0; i < 3; i++) {
+                    setPosts([])
+                }
+                console.log(error);
+            })
+    }, [setPosts, URL]);
+
+    const previousPost = () => {
+        if (postIdx > 0) {
+            setPostIdx(postIdx - 1);
+        }
+
+    }
+    const nextPost = () => {
+        if (postIdx + 3 < posts.length) {
+            setPostIdx(postIdx + 1);
+        }
+    }
+
+    return (
+        <HorizontalBlogs posts={posts} postIdx={postIdx} nextPost={nextPost} previousPost={previousPost} done={done} />
+    )
+}
+
+export default Recents
